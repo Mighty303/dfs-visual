18dc18869075608707786080ae82603eb8269b1f
Martin Wong <martinwong@Martins-MacBook-Pro.local>
2025-09-28 13:18:08 -0700

    remove solution

--- maze.py
+++ maze.py
@@ -7,23 +7,7 @@ def dfs_solve_maze_recursive(maze, start, end):
         # Base cases
         # (1) We reached the end
         # (2) Out of bounds or wall or already visited
-        if (r, c) == end:
-            path.append((r, c))
-            return True
-        if (r, c) in visited or maze[r][c] == 1:
-            return False
-        visited.add((r, c))
-        path.append((r, c))
-        for dr, dc in [(-1,0), (1,0), (0,-1), (0,1)]:
-            nr, nc = r + dr, c + dc
-            # Check bounds
-            if 0 <= nr < rows and 0 <= nc < cols:
-                # Recursively visit neighbors
-                if dfs(nr, nc):
-                    return True
-        # Backtrack
-        path.pop()
-        return False
+        pass
 
     if dfs(start[0], start[1]):
         return path
